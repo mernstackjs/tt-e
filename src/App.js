4286175b10ed22d0584f179f9c67c6a95fe4be13
Ahmed Abdirizaak <mernstack.js@gmail.com>
2022-11-11 20:34:36 +0100

    last ine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full h-screen bg-zinc-700 flex justify-center items-center text-white text-6xl">
+      App
+    </div>
+  );
 };
 
 export default App;
